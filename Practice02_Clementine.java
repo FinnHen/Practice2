470ad300c371d8ae78bec8d8e6d21d49882ec82c
FinnHen <71041428+FinnHen@users.noreply.github.com>
2023-07-06 12:06:41 -0700

    Add files via upload

---
+++ Practice02_Clementine.java
@@ -0,0 +1,46 @@
+/* 
+ * CS210 Practice 2 (Methods - Clementine)
+ * FirstName LastName - BC EmailAddress
+ * Student ID: 123 456 789
+ * MM.DD.YY - Which quarter
+ *
+ * This program writes the lyrics to "Clementine"
+ */
+
+public class Practice02_Clementine{
+
+  public static void main(String[] args) {
+
+      System.out.println("Clementine");
+      System.out.println();
+      System.out.println("In a cavern, in a canyon");
+      System.out.println("Excavating for a mine");
+      System.out.println("Lived a miner, forty-niner");
+      System.out.println("And his daughter, Clementine.");
+      System.out.println();
+      System.out.println("Oh, my darling, oh, my darling");
+      System.out.println("Oh, my darling Clementine");
+      System.out.println("You are lost and gone forever");
+      System.out.println("Dreadful sorry, Clementine.");
+      System.out.println();
+      System.out.println("Light she was and like a fairy");
+      System.out.println("And her shoes were number nine");
+      System.out.println("Herring boxes without topses");
+      System.out.println("Sandals were for Clementine");
+      System.out.println();
+      System.out.println("Oh, my darling, oh, my darling");
+      System.out.println("Oh, my darling Clementine");
+      System.out.println("You are lost and gone forever");
+      System.out.println("Dreadful sorry, Clementine.");
+      System.out.println();
+      System.out.println("Drove she ducklings to the water");
+      System.out.println("Every morning just at nine");
+      System.out.println("Hit her foot against a splinter");
+      System.out.println("Fell into the foaming brine");
+      System.out.println();
+      System.out.println("Oh, my darling, oh, my darling");
+      System.out.println("Oh, my darling Clementine");
+      System.out.println("You are lost and gone forever");
+      System.out.println("Dreadful sorry, Clementine.");
+   }
+}
